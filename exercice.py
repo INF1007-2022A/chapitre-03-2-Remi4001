19e1eff5aa8fc1f437439b64e98d9ff68e623d1e
Rémi Labelle <remilabelle@outlook.com>
2022-09-05 17:10:12 -0400

    Implémentation des fonctions dans exercice.py

--- exercice.py
+++ exercice.py
@@ -2,36 +2,52 @@
 
 
 def dissipated_power(voltage, resistance):
-	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+    return voltage ** 2 / resistance
+
 
 def orthogonal(v1, v2):
-	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+    produit_scalaire = 0
+    for i in range(len(v1)):
+        produit_scalaire += v1[i] * v2[i]
+
+    if produit_scalaire == 0:
+        return True
+    else:
+        return False
+
 
 def average(values):
-	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
-	for v in values:
-		pass # La variable v contient une valeur de la liste.
+    moyenne = 0
+
+    for v in values:
+        if v >= 0:
+            moyenne += v
+
+    return moyenne / len(values)
+
 
 def bills(value):
-	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
-	while value != 0:
-		if value >= 20:
-			pass
-		elif value >= 10:
-			pass
-		elif value >= 5:
-			pass
-		elif value >= 1:
-			pass
+    twenties = tens = fives = ones = 0
 
-	return (twenties, tens, fives, twos, ones);
+    while value != 0:
+        if value >= 20:
+            twenties += 1
+            value -= 20
+        elif value >= 10:
+            tens += 1
+            value -= 10
+        elif value >= 5:
+            fives += 1
+            value -= 5
+        elif value >= 1:
+            ones += 1
+            value -= 1
+
+    return (twenties, tens, fives, ones)
+
 
 if __name__ == "__main__":
-	print(dissipated_power(69, 420))
-	print(orthogonal((1, 1), (-1, 1)))
-	print(average([1, 4, -2, 10]))
-	print(bills(137))
+    print(dissipated_power(69, 420))
+    print(orthogonal((1, 1), (-1, 1)))
+    print(average([1, 4, -2, 10]))
+    print(bills(137))
